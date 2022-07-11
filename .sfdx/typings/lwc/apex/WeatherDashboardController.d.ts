0a860e2668268df437c68b3b6131941abd8a0598
Mitch Woloschek <mwoloschek@lpi-mwoloschek.lan>
2022-07-11 08:00:08 -0500

    .

---
+++ .sfdx/typings/lwc/apex/WeatherDashboardController.d.ts
@@ -0,0 +1,3 @@
+declare module "@salesforce/apex/WeatherDashboardController.getWeather" {
+  export default function getWeather(param: {state: any, city: any}): Promise<any>;
+}
--- force-app/main/default/lwc/weatherDashboard/weatherDashboard.js
+++ force-app/main/default/lwc/weatherDashboard/weatherDashboard.js
@@ -1,5 +1,104 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import getWeather from '@salesforce/apex/WeatherDashboardController.getWeather';
 
 export default class WeatherDashboard extends LightningElement {
+    @track showSpinner;
+    @track selectedState;
+    @track cityInput;
 
+    get stateOptions() {
+        return [
+            { label: 'Alabama', value: 'AL' },
+            { label: 'Alaska', value: 'AK' },
+            { label: 'Arizona', value: 'AZ' },
+            { label: 'Arkansas', value: 'AR' },
+            { label: 'California', value: 'CA' },
+            { label: 'Colorado', value: 'CO' },
+            { label: 'Connecticut', value: 'CT' },
+            { label: 'Delaware', value: 'DE' },
+            { label: 'District of Columbia', value: 'DC' },
+            { label: 'Florida', value: 'FL' },
+            { label: 'Georgia', value: 'GA' },
+            { label: 'Hawaii', value: 'HI' },
+            { label: 'Idaho', value: 'ID' },
+            { label: 'Illinois', value: 'IL' },
+            { label: 'Indiana', value: 'IN' },
+            { label: 'Iowa', value: 'IA' },
+            { label: 'Kansas', value: 'KS' },
+            { label: 'Kentucky', value: 'KY' },
+            { label: 'Louisiana', value: 'LA' },
+            { label: 'Maine', value: 'ME' },
+            { label: 'Maryland', value: 'MD' },
+            { label: 'Massachusetts', value: 'MA' },
+            { label: 'Minnesota', value: 'MN' },
+            { label: 'Mississippi', value: 'MS' },
+            { label: 'Missouri', value: 'MO' },
+            { label: 'Montana', value: 'MT' },
+            { label: 'Nebraska', value: 'NE' },
+            { label: 'Nevada', value: 'NV' },
+            { label: 'New Hampshire	', value: 'NH' },
+            { label: 'New Jersey', value: 'NJ' },
+            { label: 'New Mexico', value: 'NM' },
+            { label: 'New York', value: 'NY' },
+            { label: 'North Carolina', value: 'NC' },
+            { label: 'North Dakota', value: 'ND' },
+            { label: 'Ohio', value: 'OH' },
+            { label: 'Oklahoma', value: 'OK' },
+            { label: 'Oregon', value: 'OR' },
+            { label: 'Pennsylvania', value: 'PA' },
+            { label: 'Puerto Rico', value: 'PR' },
+            { label: 'Rhode Island', value: 'RI' },
+            { label: 'South Carolina', value: 'SC' },
+            { label: 'South Dakota', value: 'SD' },
+            { label: 'Tennessee', value: 'TN' },
+            { label: 'Texas', value: 'TX' },
+            { label: 'Utah', value: 'UT' },
+            { label: 'Vermont', value: 'VT' },
+            { label: 'Virginia', value: 'VA' },
+            { label: 'Virgin Islands', value: 'VI' },
+            { label: 'Washington', value: 'WA' },
+            { label: 'West Virginia', value: 'WV' },
+            { label: 'Wisconsin', value: 'WI' },
+            { label: 'Wyoming', value: 'WY' },
+        ];
+    }
+
+    handleStateSelect(event) {
+        this.selectedState = event.detail.value;
+    }
+
+    handleInputChange(event) {
+        this.cityInput = event.detail.value;
+    }
+
+    async handleClick(event) {
+        event.preventDefault();
+        this.showSpinner = true;
+
+        try {
+            const result = await getWeather({state: this.selectedState, city: this.cityInput});
+            this.showSpinner = false;
+            if (result) {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'Weather Retrieved.',
+                        variant: 'success',
+                    }),
+                );
+            }
+        } catch (e) {
+            this.showSpinner = false;
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Error',
+                    message: e.message || (e.body && e.body.message),
+                    mode: 'pester',
+                    variant: 'error',
+                }),
+            );
+        }
+
+    }
 }
